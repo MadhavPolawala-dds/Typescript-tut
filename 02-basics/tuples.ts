ad42e8c79e3b9bc98f6b9c9097c53221ae60f587
MadhavPolawala-dds <madhavpolawala.dds@gmail.com>
2024-08-15 16:10:17 +0530

    tuples in ts

---
+++ 02-basics/tuples.ts
@@ -0,0 +1,7 @@
+//// Order  of the type of array must be the same in the Tuples
+
+let user: [string, number, boolean];
+user = ["Madhav", 22, true];
+// user = [true, 23, "Madhav"];      // Not usable
+
+let rgb: [number, number, number] = [215, 155, 256];
